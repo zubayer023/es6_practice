75af56f462a43442c3139b90d12b3c90be3a79d6
Zubayer <kabbo.org@gmail.com>
2024-09-23 18:39:03 +0600

    es6 practice

---
+++ letconst.js
@@ -0,0 +1,16 @@
+const num = [40, 20];
+num[1] = 36;
+num[5] = 85;
+num.push(12);
+
+const cars = { name: "valvo", color: "black", price: "10m" };
+console.log(num);
+
+console.log(cars);
+
+let sum = 0;
+for (let i = 0; i < 10; i++) {
+  sum = sum + i;
+}
+
+console.log(i);
