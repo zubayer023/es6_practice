fb2c12fa1a7f25e9d407a14f1104a3716ff642c9
Zubayer <kabbo.org@gmail.com>
2024-09-24 19:32:50 +0600

    arrow

---
+++ arrow.js
@@ -0,0 +1,37 @@
+// function myFunction(num) {
+//   return num * 2;
+// }
+
+// const result = myFunction(5);
+// console.log(result);
+
+// const myFunction = function (num) {
+//   return num * 2;
+// };
+
+// const result = myFunction(5);
+// console.log(result);
+
+// const fruits = (mango, apple) => mango * apple;
+
+// const result = fruits(50, 2);
+
+// console.log(result);
+
+// const doMath = (x, y) => {
+//   const sum = x + y;
+//   const diff = x - y;
+
+//   const result = sum * diff;
+//   return result;
+// };
+
+// const result = doMath(50, 30);
+// console.log(result);
+
+const ages = [20, 30, 28, 22];
+const ages2 = [24, 28, 19, 27];
+
+const allAges = ages.concat(ages2);
+
+console.log(allAges);
