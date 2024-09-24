bef82b652d7c8a5cf862a7962ccd29a019249ff7
Zubayer <kabbo.org@gmail.com>
2024-09-24 09:04:57 +0600

    string use and creat multiline

---
+++ string.js
@@ -0,0 +1,7 @@
+const firstName = "Justin";
+const lastName = "Bibber";
+const fullName = firstName + lastName;
+
+const fullName2 = `${firstName} ${lastName} is a good boy`;
+
+console.log(fullName2);
