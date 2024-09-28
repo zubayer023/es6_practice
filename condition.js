2ea6cc17678f1ab9107696926bb65a1df80b5cdb
Zubayer <kabbo.org@gmail.com>
2024-09-28 22:24:23 +0600

    condition, forloop, while loop, for in, for of

---
+++ condition.js
@@ -0,0 +1,11 @@
+let age = 60;
+
+if (age <= 18) {
+  console.log("he is child");
+} else if (age >= 18 && age <= 30) {
+  console.log("adult");
+} else if (age >= 30 && age <= 40) {
+  console.log("midage");
+} else {
+  console.log("old");
+}
---
+++ forLoop.js
@@ -0,0 +1,41 @@
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i);
+
+//   if (i == 5) {
+//     break;
+//   }
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//   if (i % 2 == 1) {
+//     continue;
+//   }
+//   console.log(i);
+// }
+
+// let food = ["cake", "apple", "bannana", "ice cream"];
+// let len = food.length;
+
+// for (let i = 0; i < len; i++) {
+//   console.log(`index : ${i}`);
+//   console.log(food[i]);
+// }
+
+let name = "I am learnig javaScript";
+
+let food = ["apple", "Cake", "Ice Cream"];
+
+let person = {
+  name: "Karim",
+  profession: "Teacher",
+  country: "Bangladesh",
+  age: 24,
+};
+
+// for (const x in name) {
+//   console.log(`index ${x} and item ${name[x]}`);
+// }
+
+// for (let x in food) {
+//   console.log(`index ${x} and item ${food[x]}`);
+// }
